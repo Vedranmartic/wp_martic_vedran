763c80d4cd701ade38f7ebfd23a8e0d68edbfb51
Vedranmartic <vedranmartichr@gmail.com>
2026-01-08 22:07:23 +0100

    Zadatak 6
    JSS

---
+++ Zadaci/index.js
@@ -0,0 +1,13 @@
+let broj1 = prompt("Unesi neki broj: ");
+if(broj1>10)
+    {
+        alert(broj1 + " je veći od 10")
+    }
+else if(broj1<10)
+    {
+        alert(broj1 + " je manji od 10")
+    }
+else
+    {
+        alert(broj1 + " je jednak 10")
+    }
